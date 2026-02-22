0a993adf669fcdbb1aa34093e4f3ed3ce58fa87e
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2026-02-22 18:57:10 +0200

    samples/common/_nodetable.h: added guarded 'extern C' declarations for C++ compilers

--- samples/common/_nodetable.h
+++ samples/common/_nodetable.h
@@ -20,13 +20,17 @@
 
 #include "xbee/discovery.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define NODE_TABLE_SIZE  10
 extern xbee_node_id_t node_table[NODE_TABLE_SIZE];
 
 xbee_node_id_t *node_by_addr( const addr64 FAR *ieee_be);
 xbee_node_id_t *node_by_name( const char *name);
 xbee_node_id_t *node_by_index( int idx);
-int node_add( const xbee_node_id_t *node_id);
+ int node_add( const xbee_node_id_t *node_id);
 void node_table_dump( void);
 
 // sample_cli handlers
@@ -36,5 +40,9 @@ void handle_nodes_cmd(xbee_dev_t *xbee, char *command);
 #define NODETABLE_CLI_ENTRIES \
     { "nd",             &handle_nd_cmd },       \
     { "nodes",          &handle_nodes_cmd },    \
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
